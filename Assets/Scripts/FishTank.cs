4416f56556f9a1eca60dc791ab720961e4c589e6
Raestkjoot <gunnarmagnum@gmail.com>
2025-11-19 16:09:07 +0100

    initial project commit

---
+++ Assets/Scripts/FishTank.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class FishTank : MonoBehaviour
+{
+    private void OnMouseEnter()
+    {
+        //If your mouse hovers over the GameObject with the script attached, output this message
+        Debug.Log("Mouse is over GameObject.");
+    }
+}
---
+++ Assets/Scripts/FishType.cs
@@ -0,0 +1,6 @@
+public enum FishType
+{
+    A,
+    B,
+    C,
+}
